5da46cf6de97241d8e1c24aea87eceda80863f89
joseypan <joseypan@163.com>
2022-06-25 23:50:01 +0800

    feat: 新增14-最长公共前缀

---
+++ 14-最长公共前缀/index.js
@@ -0,0 +1,26 @@
+//【分析】
+//1、应该优先遍历第一个字符串，然后使用一个变量进行存储，然后遍历后面的字符串是否以此开头，若以此开头则表示是公共的，否则直接返回出去
+//这种方式运行速度太慢了。。。需要再次思考
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  console.log(1111);
+  //如果只有一项，则直接返回第一个字符串
+  if (strs.length === 1 || strs[0] === "") return strs[0];
+  let commonStr = "";
+  console.log(strs[0].length);
+  for (let i = 0; i < strs[0].length; i++) {
+    console.log(strs[0][i]);
+    for (let j = 1; j < strs.length; j++) {
+      console.log(strs[j], commonStr);
+      if (!strs[j].startsWith(commonStr + strs[0][i])) {
+        return commonStr;
+      }
+    }
+    commonStr += strs[0][i];
+  }
+  return commonStr;
+};
+longestCommonPrefix(["flower", "flower", "flower", "flower"]);
