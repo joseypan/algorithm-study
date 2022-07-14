cdd0164c0e3c452fbc2b77bd6caf44d24cdd8845
panxumin <joseypan@163.com>
2022-07-14 11:26:30 +0800

    feat: 新增108-将有序数组转换为二叉搜索树

---
+++ 108-将有序数组转换为二叉搜索树/index.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+// 是否需要将数组进行分组处理呢
+//【疑问】高度差不超过1，是指的判断下一个子节点存在问题？和值应该无关吧？但是和题解的答案不是很理解这个说法
+
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {};
