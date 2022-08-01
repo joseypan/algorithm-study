8892b341f086ee937d71f1e790a83831a317462f
panxumin <joseypan@163.com>
2022-08-01 15:58:47 +0800

    feat: 新增168.Excel表列名称

---
+++ 168.Excel表列名称/index.js
@@ -0,0 +1,9 @@
+// 【分析】
+// 1、目前并不知道字母与数字的对应关系，只知道A-Z是1-26,至于后面是怎么样的一个排序是不清楚的
+// 2、由于不清楚计算规则，所以也很难根据数字进行推导，不是很明白题目的含义
+// 相当于满26进一位？
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function (columnNumber) {};
