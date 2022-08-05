87b95cd4b805c5e3cafc1de472e61b0fb772da21
panxumin <joseypan@163.com>
2022-08-05 11:17:06 +0800

    perf: 优化Excel列表序号

--- 171.Excel列表序号/index.js
+++ 171.Excel列表序号/index.js
@@ -1,6 +1,7 @@
 // 【分析】
 // 1、思路大致是维护一个map用来对应字母与数字之间的关系，然后遍历传入的字符串，遍历最好是i--的形式，然后用幂次的形式进行计算
 // 公式：m*26^2+n*26^1+x*26^0
+// 可以不用太通过map,直接使用charCodeAt也能实现
 const createMap = () => {
   const map = new Map();
   map.set("A", 1);
@@ -48,6 +49,16 @@ var titleToNumber = function (columnTitle) {
     index++;
   }
   return count;
+};
+var titleToNumber = function (columnTitle) {
+  let number = 0;
+  let multiple = 1;
+  for (let i = columnTitle.length - 1; i >= 0; i--) {
+    const k = columnTitle[i].charCodeAt() - "A".charCodeAt() + 1;
+    number += k * multiple;
+    multiple *= 26;
+  }
+  return number;
 };
 const result = titleToNumber("AB");
 console.log("result", result);
