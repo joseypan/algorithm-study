b05edfd92764b9bcf30a0582138c6fe13664cd12
panxumin <joseypan@163.com>
2022-08-26 10:53:09 +0800

    perf: 优化232-用栈实现队列（双栈）

--- 232-用栈实现队列/index.js
+++ 232-用栈实现队列/index.js
@@ -40,3 +40,57 @@ MyQueue.prototype.empty = function () {
  * var param_3 = obj.peek()
  * var param_4 = obj.empty()
  */
+var MyQueue = function () {
+  this.stackPush = [];
+  this.stackPop = [];
+};
+
+MyQueue.prototype.pushToPop = function () {
+  if (this.stackPop.length === 0) {
+    while (this.stackPush.length) {
+      this.stackPop.push(this.stackPush.pop());
+    }
+  }
+};
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.stackPush.push(x);
+  this.pushToPop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (!this.stackPush.length && !this.stackPop.length) return;
+  this.pushToPop();
+  return this.stackPop.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (!this.stackPush.length && !this.stackPop.length) return;
+  this.pushToPop();
+  return this.stackPop[this.stackPop.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this.stackPop.length === 0 && this.stackPush.length === 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
