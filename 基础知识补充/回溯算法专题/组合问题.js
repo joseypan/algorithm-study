407888ebdbcf7c2377fcb453bd7ec4f98bfc1b75
panxumin <joseypan@163.com>
2022-11-03 11:24:33 +0800

    feat: 补充关于组合问题的剪枝优化

--- 基础知识补充/回溯算法专题/组合问题.js
+++ 基础知识补充/回溯算法专题/组合问题.js
@@ -24,3 +24,27 @@ const combine = (n, k) => {
   backtracking(n, k, 1, list);
   return result;
 };
+// 回溯算法的剪枝优化;
+// 举个例子，n=4,k=4的话，那么第一层for循环的时候，从元素2开始的遍历都没有意义了。在第二层for循环，从元素3开始的遍历都没有意义了
+// 剪枝的地方就是在递归中每一层的for循环所选择的起始位置。
+// 如果for循环选择的起始位置之后的元素个数已经不足我们需要的元素个数了，那么久没有必要搜索了
+// 1、已经选择的元素个数：path.length
+// 2、还需要的元素个数：k - path.length
+// 3、在集合n中至多要从该起始位置：n-(k-path.length) +1开始遍历
+const combine1 = (n, k) => {
+  const result = [];
+  const path = [];
+  const backtracking = (n, k, startIndex, path, result) => {
+    if (path.length === k) {
+      result.push([...path]);
+      return;
+    }
+    for (let i = startIndex; i <= n - (k - path.length) + 1; i++) {
+      path.push(i);
+      backtracking(n, k, i + 1, path, result);
+      path.pop();
+    }
+  };
+  backtracking(n, k, 1, path, result);
+  return result;
+};
