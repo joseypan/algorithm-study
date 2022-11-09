1393238d6d709c195a1060ca7cf44710b0804cf2
panxumin <joseypan@163.com>
2022-11-09 10:44:35 +0800

    feat: 完成131-分隔回文串

--- 131-分割回文串/index.js
+++ 131-分割回文串/index.js
@@ -5,26 +5,35 @@
 var partition = function (s) {
   const result = [];
   const path = [];
-  let letterCount = 0;
-  const backtraking = (startIndex) => {
-    //终止条件
-    if (letterCount === s.length) {
-      //说明全部都已经遍历完了
+  const isValidStr = (str, startIndex, endIndex) => {
+    while (endIndex > startIndex) {
+      if (str[startIndex] !== str[endIndex]) {
+        return false;
+      }
+      startIndex++;
+      endIndex--;
+    }
+    return true;
+  };
+  const backtracking = (startIndex) => {
+    //startIndex是当前切割符号的索引，当切割符>=s.length说明已经都切割完成了
+    if (startIndex >= s.length) {
       result.push([...path]);
       return;
     }
-    for (let i = 0; i < s.length; i++) {
-      //这里需要考虑一下怎么收集
-
-      // 若是回文
-      path.push(s[i]);
-      letterCount++;
-      backtraking(i + 1);
+    for (let i = startIndex; i < s.length; i++) {
+      //这里需要考虑什么情况下应该收集
+      if (isValidStr(s, startIndex, i)) {
+        const str = s.slice(startIndex, Math.floor(i + 1, s.length - 1));
+        path.push(str);
+      } else {
+        continue;
+      }
+      backtracking(i + 1);
       path.pop();
-      letterCount--;
     }
   };
-  backtraking(0);
+  backtracking(0);
   return result;
 };
 
