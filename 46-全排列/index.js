cf94813b9b6baa0e9c2c5b11e3f41123262f7a6a
panxumin <joseypan@163.com>
2022-11-14 10:31:08 +0800

    feat: 完成46-全排列

--- 46-全排列/index.js
+++ 46-全排列/index.js
@@ -2,24 +2,50 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// var permute = function (nums) {
+//   const result = [];
+//   const path = [];
+//   const used = [];
+//   backtracking(nums, used, result, path);
+//   return result;
+// };
+// const backtracking = (nums, used, result, path) => {
+//   if (path.length === nums.length) {
+//     result.push([...path]);
+//     return;
+//   }
+//   for (let i = 0; i < nums.length; i++) {
+//     if (used[i] === true) continue;
+//     used[i] = true;
+//     path.push(nums[i]);
+//     backtracking(nums, used, result, path);
+//     path.pop();
+//     used[i] = false;
+//   }
+// };
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
 var permute = function (nums) {
   const result = [];
   const path = [];
-  const used = [];
-  backtracking(nums, used, result, path);
+  const used = new Array(nums.length).fill(false);
+  const backtracking = () => {
+    if (path.length === nums.length) {
+      result.push([...path]);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i] === true) continue;
+      used[i] = true;
+      path.push(nums[i]);
+      backtracking();
+      path.pop();
+      used[i] = false;
+    }
+  };
+  backtracking();
   return result;
-};
-const backtracking = (nums, used, result, path) => {
-  if (path.length === nums.length) {
-    result.push([...path]);
-    return;
-  }
-  for (let i = 0; i < nums.length; i++) {
-    if (used[i] === true) continue;
-    used[i] = true;
-    path.push(nums[i]);
-    backtracking(nums, used, result, path);
-    path.pop();
-    used[i] = false;
-  }
 };
