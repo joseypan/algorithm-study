7f4a8086364ae84b8434d4b92eff9112918725e3
panxumin <joseypan@163.com>
2022-11-16 10:47:24 +0800

    fix: 使用贪心算法完成53-最大子数组和

--- 53-最大子数组和/index.js
+++ 53-最大子数组和/index.js
@@ -2,39 +2,39 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (nums) {
-  let lastMaxSum = 0;
-  let maxSum = nums[nums.length - 1];
-  //由推断可知，长度为n的数组其子项有(1+n)*n/2个
-  //所以我们需要对nums的子项进行分组来穷举表示出来，分组的规则是以当前元素开头的所有子项
-  for (let i = nums.length - 1; i >= 0; i--) {
-    if (lastMaxSum > 0) {
-      lastMaxSum += nums[i];
-    } else {
-      lastMaxSum = nums[i];
-    }
-    if (lastMaxSum > maxSum) maxSum = lastMaxSum;
-  }
-  return maxSum;
-};
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxSubArray = function (nums) {
-  let lastMaxCount = 0;
-  let maxCount = nums[0];
-  for (let i = 0; i < nums.length; i++) {
-    if (lastMaxCount > 0) {
-      lastMaxCount += nums[i];
-    } else {
-      lastMaxCount = nums[i];
-    }
-    if (maxCount < lastMaxCount) maxCount = lastMaxCount;
-  }
-  return maxCount;
-};
-maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+// var maxSubArray = function (nums) {
+//   let lastMaxSum = 0;
+//   let maxSum = nums[nums.length - 1];
+//   //由推断可知，长度为n的数组其子项有(1+n)*n/2个
+//   //所以我们需要对nums的子项进行分组来穷举表示出来，分组的规则是以当前元素开头的所有子项
+//   for (let i = nums.length - 1; i >= 0; i--) {
+//     if (lastMaxSum > 0) {
+//       lastMaxSum += nums[i];
+//     } else {
+//       lastMaxSum = nums[i];
+//     }
+//     if (lastMaxSum > maxSum) maxSum = lastMaxSum;
+//   }
+//   return maxSum;
+// };
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var maxSubArray = function (nums) {
+//   let lastMaxCount = 0;
+//   let maxCount = nums[0];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (lastMaxCount > 0) {
+//       lastMaxCount += nums[i];
+//     } else {
+//       lastMaxCount = nums[i];
+//     }
+//     if (maxCount < lastMaxCount) maxCount = lastMaxCount;
+//   }
+//   return maxCount;
+// };
+// maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
 
 /* 【分析】
 首先一个数组需要穷举其所有子数组
@@ -56,3 +56,43 @@ maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
 Smax(n) = Smax(n+1)>0?Smax(n+1) + a(n):a(n);
 这样我们可以拿到当前元素子集的最大值
 最后再把所有元素的子集最大值进行比较，拿到最大的即为数组中最大的子集和 */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  //要求出连续子数组中最大的和，一定是整数大于负数，和会更大
+  // 先暴力解法吧 -> 双层for循环 -> 但是有个问题在于，不一定是在循环之后判断大小 ->暴力解法直接超时
+  let maxCount;
+  for (let i = 0; i < nums.length; i++) {
+    let totalCount = 0;
+    for (let j = i; j < nums.length; j++) {
+      totalCount += nums[j];
+      if (maxCount === undefined || maxCount < totalCount) {
+        maxCount = totalCount;
+      }
+    }
+  }
+  return maxCount;
+};
+const result = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+console.log("result", result);
+
+// 重新思考 -> 其实可以这么想，什么情况下值最大？一定是正数和正数相加的值更大 假设A + B 只有当B是正数的时候 A+B>A
+// 难点在于如何确定B是正数呢？B可能是1个数也可能是n个数的和
+// 当发现收集终止的时候记得要复原
+var maxSubArray = function (nums) {
+  let result = -Infinity;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i];
+    if (count > result) {
+      result = count;
+    }
+    if (count < 0) {
+      count = 0;
+    }
+  }
+  return result;
+};
