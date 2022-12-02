058e4e95b44475fe68c78c541e34f00cd4afe039
panxumin <joseypan@163.com>
2022-12-02 11:41:59 +0800

    feat: 完成860-柠檬水找零

--- 860-柠檬水找零/index.js
+++ 860-柠檬水找零/index.js
@@ -5,15 +5,33 @@
  * @return {boolean}
  */
 var lemonadeChange = function (bills) {
-  let total = 0;
+  let fiveCount = 0;
+  let tenCount = 0;
   for (let i = 0; i < bills.length; i++) {
     if (bills[i] === 5) {
-      total += bills[i];
-    } else if (total < bills[i] || total % 5 !== 0) {
-      return false;
-    } else {
-      total = total - bills[i] + 5;
+      //不需要找零
+      fiveCount++;
+    } else if (bills[i] === 10) {
+      //消耗一张五块的，增加一张十块的
+      if (fiveCount > 0) {
+        fiveCount -= 1;
+        tenCount++;
+      } else {
+        return false; //没有五块的，无法找零
+      }
+    } else if (bills[i] === 20) {
+      //优先考虑一张10块一张5块
+      if (tenCount > 0 && fiveCount > 0) {
+        tenCount -= 1;
+        fiveCount -= 1;
+      } else if (fiveCount >= 3) {
+        fiveCount -= 3;
+      } else {
+        return false;
+      }
     }
   }
   return true;
 };
+const result = lemonadeChange([5, 5, 5, 10, 20]);
+console.log("result", result);
