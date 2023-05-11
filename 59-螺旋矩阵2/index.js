7a4e09646299f9cd8f7a4b1d103a6f098e9277f7
joseypan <joseypan@163.com>
2023-05-11 22:39:59 +0800

    feat: 完成59-螺旋矩阵2

--- 59-螺旋矩阵2/index.js
+++ 59-螺旋矩阵2/index.js
@@ -2,4 +2,35 @@
  * @param {number} n
  * @return {number[][]}
  */
-var generateMatrix = function (n) {};
+var generateMatrix = function (n) {
+  const ans = new Array(n).fill(0).map((ele) => new Array(n).fill(0));
+  let startX = 0;
+  let startY = 0;
+  let loop = Math.floor(n / 2);
+  let middle = Math.floor(n / 2);
+  let offset = 1;
+  let count = 1;
+  let i, j;
+
+  while (loop--) {
+    for (j = startY; j < n - offset; j++) {
+      ans[startX][j] = count++;
+    }
+    for (i = startX; i < n - offset; i++) {
+      ans[i][j] = count++;
+    }
+    for (; j > startY; j--) {
+      ans[i][j] = count++;
+    }
+    for (; i > startX; i--) {
+      ans[i][j] = count++;
+    }
+    startX++;
+    startY++;
+    offset++;
+  }
+  if (n % 2 === 1) {
+    ans[middle][middle] = count++;
+  }
+  return ans;
+};
