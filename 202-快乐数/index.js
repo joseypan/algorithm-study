5bc1cc38681944cd8ed0528ca6e221dc35e0917d
panxumin <joseypan@163.com>
2023-05-15 13:30:43 +0800

    fix: 重新修改202-快乐数

--- 202-快乐数/index.js
+++ 202-快乐数/index.js
@@ -26,3 +26,25 @@ function calculate(n) {
 
 const result = isHappy(2);
 console.log("result", result);
+
+// 一旦这个数之前出现过了，则说明进入循环了
+var isHappy = function (n) {
+  const set = new Set();
+  let num = n;
+  while (num !== 1) {
+    // 需要一位一位的取
+    let curCount = num;
+    let total = 0;
+    while (Math.floor(curCount) > 0) {
+      const number = curCount % 10;
+      curCount = Math.floor(curCount / 10);
+      total += number * number;
+    }
+    // 加入set，看size是否变化
+    const setSize = set.size;
+    num = total;
+    set.add(num);
+    if (setSize === set.size) return false;
+  }
+  return true;
+};
